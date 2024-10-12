78dd730432e118bddc044d1cf12add87093860fb
Ian Unruh <me@ianunruh.com>
2024-10-12 17:53:51 -0600

    Implement more MAC/IP args

--- formatter.go
+++ formatter.go
@@ -12,6 +12,9 @@ import (
 func formatArg(arg Arg, hint DisplayHint, v []byte) (string, error) {
 	switch arg {
 	case I8Arg:
+		if len(v) != 1 {
+			return "", fmt.Errorf("expected i8 arg to be exactly 1 byte: %v", v)
+		}
 		return formatInt(hint, int8(v[0]))
 	case I16Arg:
 		return formatInt(hint, int16(binary.LittleEndian.Uint16(v)))
@@ -20,6 +23,9 @@ func formatArg(arg Arg, hint DisplayHint, v []byte) (string, error) {
 	case I64Arg, IsizeArg:
 		return formatInt(hint, int64(binary.LittleEndian.Uint64(v)))
 	case U8Arg:
+		if len(v) != 1 {
+			return "", fmt.Errorf("expected u8 arg to be exactly 1 byte: %v", v)
+		}
 		return formatInt(hint, uint8(v[0]))
 	case U16Arg:
 		return formatInt(hint, binary.LittleEndian.Uint16(v))
@@ -36,11 +42,20 @@ func formatArg(arg Arg, hint DisplayHint, v []byte) (string, error) {
 	case IPv6AddrArg:
 		// TODO implement me
 	case ArrU8Len4Arg:
-		// TODO implement me
+		if len(v) != 4 {
+			return "", fmt.Errorf("expected ArrU8Len4 arg to be exactly 4 bytes: %v", v)
+		}
+		return formatArrU8Len4Arg(hint, [4]byte(v))
 	case ArrU8Len6Arg:
-		// TODO implement me
+		if len(v) != 6 {
+			return "", fmt.Errorf("expected ArrU8Len6 arg to be exactly 6 bytes: %v", v)
+		}
+		return formatArrU8Len6Arg(hint, [6]byte(v))
 	case ArrU8Len16Arg:
-		// TODO implement me
+		if len(v) != 16 {
+			return "", fmt.Errorf("expected ArrU8Len16 arg to be exactly 16 bytes: %v", v)
+		}
+		return formatArrU8Len16Arg(hint, [16]byte(v))
 	case ArrU16Len8Arg:
 		// TODO implement me
 	case BytesArg:
@@ -60,6 +75,32 @@ func formatU32Arg(hint DisplayHint, v uint32) (string, error) {
 		return formatIPv4Addr(v), nil
 	}
 	return formatInt(hint, v)
+}
+
+func formatArrU8Len4Arg(hint DisplayHint, v [4]byte) (string, error) {
+	switch hint {
+	case IPHint:
+		return netip.AddrFrom4(v).String(), nil
+	}
+	return "", fmt.Errorf("unsupported display hint: %d", hint)
+}
+
+func formatArrU8Len6Arg(hint DisplayHint, v [6]byte) (string, error) {
+	switch hint {
+	case LowerMACHint:
+		return formatLowerMAC(v), nil
+	case UpperMACHint:
+		return formatUpperMAC(v), nil
+	}
+	return "", fmt.Errorf("unsupported display hint: %d", hint)
+}
+
+func formatArrU8Len16Arg(hint DisplayHint, v [16]byte) (string, error) {
+	switch hint {
+	case IPHint:
+		return netip.AddrFrom16(v).String(), nil
+	}
+	return "", fmt.Errorf("unsupported display hint: %d", hint)
 }
 
 func formatFloat(hint DisplayHint, v any) (string, error) {
@@ -96,6 +137,16 @@ func formatIPv4Addr(v uint32) string {
 	b := [4]byte{}
 	binary.BigEndian.PutUint32(b[:], v)
 	return netip.AddrFrom4(b).String()
+}
+
+func formatLowerMAC(v [6]byte) string {
+	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x",
+		v[0], v[1], v[2], v[3], v[4], v[5])
+}
+
+func formatUpperMAC(v [6]byte) string {
+	return fmt.Sprintf("%02X:%02X:%02X:%02X:%02X:%02X",
+		v[0], v[1], v[2], v[3], v[4], v[5])
 }
 
 func formatLowerHex(v any) string {
